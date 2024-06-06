a3b25bf993b45758a29d11cb1b98bc2a2ff539fa
Chris <chrisaconquet@hotmail.com>
2024-06-06 17:59:14 +0200

    Update SpiralRotator.cs
    Change in input

--- Assets/Game/Scripts/SpiralRotator.cs
+++ Assets/Game/Scripts/SpiralRotator.cs
@@ -10,18 +10,18 @@ public class SpiralRotator : MonoBehaviour
     private void Update() {
 
         // PC Controls
-        #if UNITY_EDITOR
-            if(Input.GetMouseButton(0)){
-                float mouseX = Input.GetAxisRaw("Mouse X");
-                transform.Rotate (transform.position.x, -mouseX * rotationSpeedPC * Time.deltaTime, transform.position.z);
-            }
+        
+        if(Input.GetMouseButton(0)){
+            float mouseX = Input.GetAxisRaw("Mouse X");
+            transform.Rotate (transform.position.x, -mouseX * rotationSpeedPC * Time.deltaTime, transform.position.z);
+        }
         
         // Android/Iphone Controls
-        #elif UNITY_ANDROID
-        if(Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Moved){
-            float xDeltaPos = Input.GetTouch(0).deltaPosition.x;
-            transform.Rotate(transform.position.x, -xDeltaPos * rotationSpeedDevice * Time.deltaTime, transform.position.z);
-        }
+        #if UNITY_ANDROID
+            if(Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Moved){
+                float xDeltaPos = Input.GetTouch(0).deltaPosition.x;
+                transform.Rotate(transform.position.x, -xDeltaPos * rotationSpeedDevice * Time.deltaTime, transform.position.z);
+            }
         #endif
     }
 }
